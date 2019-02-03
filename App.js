f1611276cccbfdd706c014de168c0ba60a8667b2
Ivan Tarin <ivantarin@Ivans-MacBook-Pro.local>
2019-02-02 20:46:58 -0700

    added delte feature

--- App.js
+++ App.js
@@ -25,13 +25,26 @@ export default class App extends Component {
       }
     });
   }; 
+
+  placeDeletedHandler = index => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
+      }
+    })
+  }
     
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.large}>{this.state.places}</Text>
         <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-        <PlaceList places={this.state.places} />
+        <PlaceList 
+          places={this.state.places} 
+          onItemDeleted={this.placeDeletedHandler} 
+        />
     </View>
     );
   }
